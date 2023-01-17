fce6e36f381496c1cc906e0f1a7a0317c889693b
d.xasanshin@bk.ru <kukubik>
2023-01-17 18:00:29 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using CalculationArea2DFigure;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Triangle triangle = new Triangle(2, 5, 5);
+
+            Console.WriteLine(triangle.IsRectangular());
+            
+            Console.WriteLine(triangle.GetSquare());
+
+            Console.WriteLine(triangle.GetHalfMeter());
+        }
+    }
+}
