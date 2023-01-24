8c5d28a95cdecc785994c14d9afdfcdb783fa022
Damir <81904017+damire-da@users.noreply.github.com>
2023-01-24 19:02:19 +0000

    Add files via upload

---
+++ CalculationArea2DFigure/Shape.cs
@@ -0,0 +1,17 @@
+﻿using CalculationArea2DFigure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.CalculationArea2DFigure
+{
+    public static class Shape
+    {
+        public static double GetSquare(Figure figure)
+        {
+            return figure.GetSquare();
+        }
+    }
+}
